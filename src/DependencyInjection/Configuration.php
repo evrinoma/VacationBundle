ef0ff9196ef78278bf9869f91a0eb7701ab049fd
Nikolay Nikolaev <evrinoma@gmail.com>
2021-12-02 18:40:14 +0300

    DI and params

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,6 +18,27 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder      = new TreeBuilder(EvrinomaVacationBundle::VACATION_BUNDLE);
         $rootNode         = $treeBuilder->getRootNode();
+        $supportedDrivers = ['orm'];
+
+        $rootNode
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('db_driver')
+            ->validate()
+            ->ifNotInArray($supportedDrivers)
+            ->thenInvalid('The driver %s is not supported. Please choose one of '.json_encode($supportedDrivers))
+            ->end()
+            ->cannotBeOverwritten()
+            ->defaultValue('orm')
+            ->end()
+            ->scalarNode('factory_vacation')->cannotBeEmpty()->defaultValue(EvrinomaVacationExtension::FACTORY_VACATION)->end()
+            ->scalarNode('entity_vacation')->cannotBeEmpty()->defaultValue(EvrinomaVacationExtension::ENTITY_BASE_VACATION)->end()
+            ->scalarNode('constraints_vacation')->defaultTrue()->info('This option is used for enable/disable basic vacation constraints')->end()
+            ->scalarNode('dto_vacation')->cannotBeEmpty()->defaultValue(EvrinomaVacationExtension::DTO_BASE_VACATION)->info('This option is used for dto class override')->end()
+            ->arrayNode('decorates')->addDefaultsIfNotSet()->children()
+            ->scalarNode('command_vacation')->defaultNull()->info('This option is used for command vacation decoration')->end()
+            ->scalarNode('query_vacation')->defaultNull()->info('This option is used for query vacation decoration')->end()
+            ->end()->end()->end();
 
         return $treeBuilder;
     }
--- src/DependencyInjection/EvrinomaVacationExtension.php
+++ src/DependencyInjection/EvrinomaVacationExtension.php
@@ -3,21 +3,145 @@
 namespace Evrinoma\VacationBundle\DependencyInjection;
 
 use Evrinoma\UtilsBundle\DependencyInjection\HelperTrait;
+use Evrinoma\VacationBundle\Dto\VacationApiDto;
 use Evrinoma\VacationBundle\EvrinomaVacationBundle;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 
 class EvrinomaVacationExtension extends Extension
 {
     use HelperTrait;
 
+    public const ENTITY           = 'Evrinoma\VacationBundle\Entity';
+    public const FACTORY_VACATION     = 'Evrinoma\VacationBundle\Factory\VacationFactory';
+    public const ENTITY_BASE_VACATION = self::ENTITY.'\Vacation\BaseVacation';
+    public const DTO_BASE_VACATION    = VacationApiDto::class;
+
+    /**
+     * @var array
+     */
+    private static array $doctrineDrivers = array(
+        'orm' => array(
+            'registry' => 'doctrine',
+            'tag'      => 'doctrine.event_subscriber',
+        ),
+    );
 //region SECTION: Public
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        if ($container->getParameter('kernel.environment') !== 'prod') {
+            $loader->load('fixtures.yml');
+        }
+
+        $configuration = $this->getConfiguration($configs, $container);
+        $config        = $this->processConfiguration($configuration, $configs);
+
+        if ($config['factory_vacation'] !== self::FACTORY_VACATION) {
+            $this->wireFactory($container, 'vacation', $config['factory_vacation'], $config['entity_vacation']);
+        } else {
+            $definitionFactory = $container->getDefinition('evrinoma.'.$this->getAlias().'.vacation.factory');
+            $definitionFactory->setArgument(0, $config['entity_vacation']);
+        }
+
+
+        $doctrineRegistry = null;
+
+        if (isset(self::$doctrineDrivers[$config['db_driver']])) {
+            $loader->load('doctrine.yml');
+            $container->setAlias('evrinoma.'.$this->getAlias().'.doctrine_registry', new Alias(self::$doctrineDrivers[$config['db_driver']]['registry'], false));
+            $doctrineRegistry = new Reference('evrinoma.'.$this->getAlias().'.doctrine_registry');
+            $container->setParameter('evrinoma.'.$this->getAlias().'.backend_type_'.$config['db_driver'], true);
+            $objectManager = $container->getDefinition('evrinoma.'.$this->getAlias().'.object_manager');
+            $objectManager->setFactory([$doctrineRegistry, 'getManager']);
+        }
+
+        $this->remapParametersNamespaces(
+            $container,
+            $config,
+            [
+                '' => [
+                    'db_driver'   => 'evrinoma.'.$this->getAlias().'.storage',
+                    'entity_vacation' => 'evrinoma.'.$this->getAlias().'.entity_vacation',
+                ],
+            ]
+        );
+
+        if ($doctrineRegistry) {
+            $this->wireRepository($container, $doctrineRegistry, 'vacation', $config['entity_vacation']);
+        }
+
+        $this->wireController($container, 'vacation', $config['dto_vacation']);
+
+        $this->wireValidator($container, 'vacation', $config['entity_vacation']);
+
+        $loader->load('validation.yml');
+
+        if ($config['constraints_vacation']) {
+            $loader->load('constraint/vacation.yml');
+        }
+
+        $this->wireConstraintTag($container);
+
+        if ($config['decorates']) {
+            $this->remapParametersNamespaces(
+                $container,
+                $config['decorates'],
+                [
+                    '' => [
+                        'command_vacation' => 'evrinoma.'.$this->getAlias().'.decorates.vacation.command',
+                        'query_vacation'   => 'evrinoma.'.$this->getAlias().'.decorates.vacation.query',
+                    ],
+                ]
+            );
+        }
+    }
+
+    private function wireConstraintTag(ContainerBuilder $container): void
+    {
+
+    }
+
+    private function wireFactory(ContainerBuilder $container, string $name, string $class, string $paramClass): void
+    {
+        $container->removeDefinition('evrinoma.'.$this->getAlias().'.'.$name.'.factory');
+        $definitionFactory = new Definition($class);
+        $definitionFactory->addArgument($paramClass);
+        $alias = new Alias('evrinoma.'.$this->getAlias().'.'.$name.'.factory');
+        $container->addDefinitions(['evrinoma.'.$this->getAlias().'.'.$name.'.factory' => $definitionFactory]);
+        $container->addAliases([$class => $alias]);
+    }
+
+    private function wireController(ContainerBuilder $container, string $name, string $class): void
+    {
+        $definitionApiController = $container->getDefinition('evrinoma.'.$this->getAlias().'.'.$name.'.api.controller');
+        $definitionApiController->setArgument(5, $class);
+    }
+
+    private function wireValidator(ContainerBuilder $container, string $name, string $class): void
+    {
+        $definitionApiController = $container->getDefinition('evrinoma.'.$this->getAlias().'.'.$name.'.validator');
+        $definitionApiController->setArgument(0, $class);
+    }
+
+    private function wireRepository(ContainerBuilder $container, Reference $doctrineRegistry, string $name, string $class): void
+    {
+        $definitionRepository    = $container->getDefinition('evrinoma.'.$this->getAlias().'.'.$name.'.repository');
+        $definitionQueryMediator = $container->getDefinition('evrinoma.'.$this->getAlias().'.'.$name.'.query.mediator');
+        $definitionRepository->setArgument(2, $definitionQueryMediator);
+        $definitionRepository->setArgument(1, $class);
+        $definitionRepository->setArgument(0, $doctrineRegistry);
+
+        $array = $definitionRepository->getArguments();
+        ksort($array);
+        $definitionRepository->setArguments($array);
     }
 //endregion Public
 
