04a75e7d291be772cfd136eb5eff71799d36bc0c
Nikolay Nikolaev <evrinoma@gmail.com>
2021-12-02 18:30:57 +0300

    Vacation SuperClass description

--- src/Model/Vacation/AbstractVacation.php
+++ src/Model/Vacation/AbstractVacation.php
@@ -2,16 +2,79 @@
 
 namespace Evrinoma\VacationBundle\Model\Vacation;
 
-use Evrinoma\UtilsBundle\Entity\CreateUpdateAtTrait;
-use Evrinoma\UtilsBundle\Entity\IdTrait;
 use Doctrine\ORM\Mapping as ORM;
+use Evrinoma\UtilsBundle\Entity\CreateUpdateAtTrait;
+use Evrinoma\UtilsBundle\Entity\DateStartFinishTrait;
+use Evrinoma\UtilsBundle\Entity\IdTrait;
+use Evrinoma\VacationBundle\Model\Status\StatusTrait;
+use Evrinoma\VacationBundle\Model\User\VacationInterface as UserVacationInterface;
 
 /**
  * @ORM\MappedSuperclass
- * @TODO think about restrictions 
- * ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="idx_vacation", columns={"created"})})
+ *
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="idx_vacation", columns={"user_id","dateStart","dateFinish"})})
  */
 abstract class AbstractVacation implements VacationInterface
 {
-    use IdTrait, CreateUpdateAtTrait;
+    use IdTrait, CreateUpdateAtTrait, DateStartFinishTrait, StatusTrait;
+
+//region SECTION: Fields
+    /**
+     * @var UserVacationInterface
+     *
+     * @ORM\ManyToOne(targetEntity="Evrinoma\VacationBundle\Model\Define\UserVacationInterface")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    protected UserVacationInterface $user;
+
+    /**
+     * @var UserVacationInterface
+     *
+     * @ORM\ManyToOne(targetEntity="Evrinoma\VacationBundle\Model\Define\UserVacationInterface")
+     * @ORM\JoinColumn(name="resolver_id", referencedColumnName="id")
+     */
+    protected UserVacationInterface $resolver;
+//endregion Fields
+
+//region SECTION: Getters/Setters
+    /**
+     * @return UserVacationInterface
+     */
+    public function getUser(): UserVacationInterface
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return UserVacationInterface
+     */
+    public function getResolver(): UserVacationInterface
+    {
+        return $this->resolver;
+    }
+
+    /**
+     * @param UserVacationInterface $user
+     *
+     * @return VacationInterface
+     */
+    public function setUser(UserVacationInterface $user): VacationInterface
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @param UserVacationInterface $resolver
+     *
+     * @return VacationInterface
+     */
+    public function setResolver(UserVacationInterface $resolver): VacationInterface
+    {
+        $this->resolver = $resolver;
+
+        return $this;
+    }
+//endregion Getters/Setters
 }
--- src/Model/Vacation/VacationInterface.php
+++ src/Model/Vacation/VacationInterface.php
@@ -4,9 +4,11 @@
 namespace Evrinoma\VacationBundle\Model\Vacation;
 
 use Evrinoma\UtilsBundle\Entity\CreateUpdateAtInterface;
+use Evrinoma\UtilsBundle\Entity\DateStartFinishInterface;
 use Evrinoma\UtilsBundle\Entity\IdInterface;
+use Evrinoma\VacationBundle\Model\Status\StatusInterface;
 
-interface VacationInterface extends CreateUpdateAtInterface, IdInterface
+interface VacationInterface extends CreateUpdateAtInterface, IdInterface, DateStartFinishInterface, StatusInterface
 {
 
 }
