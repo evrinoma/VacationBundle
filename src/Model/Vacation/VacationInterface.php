8574528f4c94d73edcc4141947d1469c2919e921
Nikolay Nikolaev <evrinoma@gmail.com>
2021-12-02 23:17:02 +0300

    fix SuperClass Vacation

--- src/Model/Vacation/VacationInterface.php
+++ src/Model/Vacation/VacationInterface.php
@@ -7,8 +7,33 @@ use Evrinoma\UtilsBundle\Entity\CreateUpdateAtInterface;
 use Evrinoma\UtilsBundle\Entity\DateStartFinishInterface;
 use Evrinoma\UtilsBundle\Entity\IdInterface;
 use Evrinoma\VacationBundle\Model\Status\StatusInterface;
+use Evrinoma\VacationBundle\Model\User\VacationInterface as UserVacationInterface;
 
 interface VacationInterface extends CreateUpdateAtInterface, IdInterface, DateStartFinishInterface, StatusInterface
 {
+//region SECTION: Getters/Setters
+    /**
+     * @return UserVacationInterface
+     */
+    public function getResolver(): UserVacationInterface;
 
+    /**
+     * @return UserVacationInterface
+     */
+    public function getUser(): UserVacationInterface;
+
+    /**
+     * @param UserVacationInterface $user
+     *
+     * @return VacationInterface
+     */
+    public function setUser(UserVacationInterface $user): VacationInterface;
+
+    /**
+     * @param UserVacationInterface $resolver
+     *
+     * @return VacationInterface
+     */
+    public function setResolver(UserVacationInterface $resolver): VacationInterface;
+//endregion Getters/Setters
 }
